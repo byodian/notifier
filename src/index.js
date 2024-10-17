9b913a7621451c1e2a900b93c47c309ec2101afc
Yongjian Bai <byj6696@gmail.com>
2024-10-17 11:10:46 +0800

    feat: add github star notification

--- src/index.js
+++ src/index.js
@@ -1,15 +1,59 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 export default {
 	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
-	},
-};
+		if (request.method !== 'POST') {
+			return new Response('Invalid request method', { status: 405 })
+		}
+
+		const githubEvent = request.headers.get('X-GitHub-Event')
+    try {
+			const body = await request.json()
+
+			// 构建发送到 Telegram 的消息内容
+			let message = "Hello world"
+			if (githubEvent === 'watch') {
+				message = makeMessage(body)
+			}
+
+			// 发送消息到 Telegram
+			const telegramUrl = `https://api.telegram.org/bot${env.TELEGRAM_TOKEN}/sendMessage`
+
+			const telegramPayload = {
+				chat_id: env.TELEGRAM_CHAT_ID,
+				text: message,
+				parse_mode: "Markdown"
+			}
+
+			const telegramResponse = await fetch(telegramUrl, {
+				method: 'POST',
+				headers: { 'Content-Type': 'application/json' },
+				body: JSON.stringify(telegramPayload),
+			})
+
+			if (telegramResponse.ok) {
+				return new Response('Message sent to Telegram', { status: 200 })
+			} else {
+				console.log(telegramResponse)
+				return new Response(`Failed to send message to Telegram`, { status: 500 })
+			}
+    } catch (error) {
+      return new Response("Failed to process webhook event.", {
+        status: 500,
+      });
+    }
+	}	
+}
+
+function makeMessage(body) {
+	const repo = body.repository.repository
+	const repoUrl = body.repository.html_url
+	const stars = body.repository.stargazers_count
+	const forks = body.repository.forks_count
+	const sender = body.sender.login
+	const senderUrl = body.sender.html_url
+
+	return `
+New Github star for [${repo}](${repoUrl}) repo!
+The **${repo}** repo now has **${stars}** starts and **${forks}** forks!🎉
+Your new fan is [${sender}](${senderUrl})
+`
+}
--- test/index.spec.js
+++ test/index.spec.js
@@ -4,13 +4,13 @@ import worker from '../src';
 
 describe('Hello World worker', () => {
 	it('responds with Hello World! (unit style)', async () => {
-		const request = new Request('http://example.com');
+		const request = new Request('http://example.com', { method: 'POST'});
 		// Create an empty context to pass to `worker.fetch()`.
 		const ctx = createExecutionContext();
 		const response = await worker.fetch(request, env, ctx);
 		// Wait for all `Promise`s passed to `ctx.waitUntil()` to settle before running test assertions
 		await waitOnExecutionContext(ctx);
-		expect(await response.text()).toMatchInlineSnapshot(`"Hello World!"`);
+		expect(await response.text()).toMatchInlineSnapshot(`"Hello World"`);
 	});
 
 	it('responds with Hello World! (integration style)', async () => {
